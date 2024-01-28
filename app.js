bc11fb79a6e349e4d5493ee7542b8084b848d420
Steven <Huwan.Studios@gmail.com>
2024-01-28 02:51:13 -0500

    Updated tail + completed alert issue

--- app.js
+++ app.js
@@ -5,10 +5,19 @@
 //  - a cost
 //  - a quantity
 
-// const productsName = prompt("Enter  ");
-// const model = prompt("Enter  ");
-// const cost = prompt("Enter  ");
-// const qty = prompt("Enter  ");
+
+// Approach 1 -
+// Create an array to hold the inventory - think`let`)
+// PART 1
+let inventory = [];
+console.log(inventory);
+// console.log(inventory[0]);
+
+
+const productsName = prompt("Enter  ");
+const model = prompt("Enter  ");
+const cost = prompt("Enter  ");
+const qty = prompt("Enter  ");
 
 inventory.push(productsName);
 inventory.push(model);
@@ -21,10 +30,10 @@ console.log(qty);
 
 
 
-// const productsName2 = prompt("Enter  ");
-// const model2 = prompt("Enter  ");
-// const cost2 = prompt("Enter  ");
-// const qty2 = prompt("Enter  ");
+const productsName2 = prompt("Enter  ");
+const model2 = prompt("Enter  ");
+const cost2 = prompt("Enter  ");
+const qty2 = prompt("Enter  ");
 
 inventory.push(productsName2);
 inventory.push(model2);
@@ -36,10 +45,10 @@ console.log(cost2);
 console.log(qty2);
 
 
-// const productsName3 = prompt("Enter  ");
-// const model3 = prompt("Enter  ");
-// const cost3 = prompt("Enter  ");
-// const qty3 = prompt("Enter  ");
+const productsName3 = prompt("Enter  ");
+const model3 = prompt("Enter  ");
+const cost3 = prompt("Enter  ");
+const qty3 = prompt("Enter  ");
 
 inventory.push(productsName3);
 inventory.push(model3);
@@ -51,10 +60,10 @@ console.log(cost3);
 console.log(qty3);
 
 
-// const productsName4 = prompt("Enter  ");
-// const model4 = prompt("Enter  ");
-// const cost4 = prompt("Enter  ");
-// const qty4 = prompt("Enter  ");
+const productsName4 = prompt("Enter  ");
+const model4 = prompt("Enter  ");
+const cost4 = prompt("Enter  ");
+const qty4 = prompt("Enter  ");
 
 inventory.push(productsName4);
 inventory.push(model4);
@@ -80,38 +89,29 @@ console.log(model5);
 console.log(cost5);
 console.log(qty5);
 
-// Approach 1 -
-// Create an array to hold the inventory - think`let`)
-// PART 1
-let inventory = [];
-console.log(inventory);
-// console.log(inventory[0]);
 
-// let productCatalog = [];
+let productCatalog = [
+  [productsName, model, cost, qty],
+  [productsName2, model2, cost2, qty2],
+  [productsName3, model3, cost3, qty3],
+  [productsName4, model4, cost4, qty4],
+  [productsName5, model5, cost5, qty5],
 
-// let productCatalog = [
-//   [productsName, model, cost, qty],
-//   [productsName2, model2, cost2, qty2],
-//   [productsName3, model3, cost3, qty3],
-//   [productsName4, model4, cost4, qty4],
-//   [productsName5, model5, cost5, qty5],
-
-// ];
-// console.log(productCatalog);
+];
+console.log(productCatalog);
 
 // // With a loop, check for the size of your array
-// for (
-//   myChecker = 0;
-//   myChecker < 5;
-//   myChecker = myChecker + 1;
-// ) {
+for (
+  myChecker = 0;
+  myChecker < 5;
+  myChecker = myChecker + 1
+) {
 
-//   console.log(myChecker);
-//   console.log(inventory[myChecker]);
-// };
+  console.log(myChecker);
+  console.log(productCatalog[myChecker]);
+};
 
 
 // for (let inventory){}
-
-
-
+//alert
+// alert(productCatalog[myChecker])
